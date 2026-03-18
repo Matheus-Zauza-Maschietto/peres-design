d28d82cbac90118d54366616e8a243cfbd06b6de
Matheus Zauza Maschietto <matheus_maschietto@yahoo.com.br>
2026-03-17 22:32:54 -0300

    Refactorig and adding Giselle images

--- src/components/PortfolioGallerySection/PortfolioGallerySection.jsx
+++ src/components/PortfolioGallerySection/PortfolioGallerySection.jsx
@@ -1,73 +1,9 @@
 import { useState } from "react";
 import ProjectCard from "./Components/ProjectCard";
 import ProjectSidebar from "./Components/ProjectSidebar";
+import { projectsService } from "../../services/ProjectsService";
 
-const projects = [
-  {
-    id: 1,
-    title: "Giselle Moraes Confeitaria",
-    category: "Identidade Visual Confeitaria Gourmet",
-    image: "https://lh3.googleusercontent.com/rd-d/ALs6j_Fh_TpPpLwGj3Vc8_rlMC4MBR2QpkddlUvIX2iILdeGHrddjwpbk42GpfKZQSc3m5R3aPLBplOMV7vvqcpy0IHjhsbAjQl1H3JLISKwTWXbYZZOL3ew_EOjFcyCi53ajl5EKUdYbxKEcv65B83wUUTs25cJ9CkgJwpdF3mqI8bO2gGbqOtm3GdNneQLJZ7-MccNQ0dNT0hyUkOZPWDmDOE2m7U9so7cos02POglExq5jix8iIRGDECvFWUlxasCq3Hky_lUYv50IYfKjEc2sTvdWG_Pp12dHu1yqbGbxBShxsCvCn2dFJE2fESUgFNBL0vA-V2qge2m6739lqiZimLh6MTysPc0wSafjDqbbf3g5dRSXHwdALk6sOY5n42QRyLh176xH_6sf1AL9fTgfExaDRMB6BWatsFkCBNUXN-13d1g-eGb3VnlDik14gBhQoIUj8ClDbTSrMZvoprqAJw6u8LeFLWjCfQjHRmKOqN9rfCdjpRWh5n9PYze2yd_uQ7mmWmnCqOXZm2WNMj8ECek3h7aelt1io2CvI0SFZ4id-FBgmMAverQSAYW7X6d4k_BBNmzydHVKXTGRlU78jQM-qo-FGquUxf6uZsj1hxD8QbF8-cd6LQ0VIItkdL3CdKUBNmsWk0e3YAil3yIg3EHfI62rS3oR6Tja9CgRdCbN1HqQ3uLxWID6e3feR94cmGjHiWX7k14Ayj-PqGa7ZUi7fZIWCbGdthTT66nQqcQFvPayNjO5VrgJR9ibqSClU_w9BQHZ6VrY1znLdR7QS8o1lIcROLJKXo4kmI9ZFhjpWmWZFVW0XuD3lSp2YDSDy761QO07Nh8SZPonCIQxX9Kam-YDz3s5vb13sox7A52Dx-VUkz-NFJmHnn-puadGqb3eIOhAFptYQcXbIV-HNjY9zpDEDs6giu97jbYmGGLJgaUj6mSsunCLPuI9fIcdxYf63bmxDVGeVLVlaZp5l8aDrze9yKqaZdFHLmu-QGq3O9e2ChcUF_1lR3Txo08_tYaPmDB8ImvgOCADnhdoBsRrgstbYvdJxE0IE30egQ=w1261-h916?auditContext=forDisplay",
-    imageMatrix: [
-      ["https://dummyimage.com/1200x700/1a1a1a/ffffff"],
-      [
-        "https://dummyimage.com/600x750/1a1a1a/ffffff",
-        "https://dummyimage.com/600x750/1a1a1a/ffffff",
-      ],
-      ["https://dummyimage.com/1200x800/1a1a1a/ffffff"],
-    ],
-  },
-  {
-    id: 2,
-    title: "Lev Cream | Delicias Gourmet",
-    category: "Identidade Visual Confeitaria Gourmet",
-    image: "https://lh3.googleusercontent.com/rd-d/ALs6j_HyibUR7nBq-MM4xfDDpvOoPErlpMk3-IukujhYHagoBJ4MSvH0E-byBQoCZu7erO2MFz1hJlHvO-NQJ0BJ8Img9dEjtFB8RSE8hLmWn0-DguykPL3lwoLmb1r5hoCO7UMUNsoqxe9a1lSJGJRr-_UXoSyjV2GXaSrG9uJCAC8t8DeiWVFZYI-zxwCyVTMwrnw9AY8QloFLEWIt4whfudL_NBN-8knFqyxOEoabJ8HhS54rFvzRAkrLvdOHPjtfd4BTWV1oD7zBbRvclnjzQNmyCL8teXslpxk9UnNPTrGFkpJZbaqHgvJkYRBlHqhNyHrpmdJ6pfj8BOCtTpep3CZ87qGOgfLOABHL0McBGQ6LO_Zbbm-Rf5660lO_5n-mc91fSXhEcSqj9p_J8ryVoESxtZFDFyYy21mANE6fVmgkn2CGAbx5oX6Dfm_iUAkz-LWZWwAqItxZ1s1QlcTcXzfILaOZJjDiTVrmT90hC63OyuAGc0Zglld6pBe371j_q2Gg53Ena7kG7k2xEsuzxh2X9MDOWQbyyooTaUsiL4EmznzK9BdqZFVBVyf5qQdVv5TidXnGG_I2nHLLDAy93G6igitMgTED2Hh7QTcJyqiYE8ddHmSA_xDSe6PNajp-sHsn3MTD9iaIT7rv31l7WPFla3x7WQ8mLLUMezcPbR4Pc_PLfraswLra-UpMoZATVf9-q3xz-vXHks5vwNtx1EZ8EU6O7QJfiZVS5Io4FKnHHEhLsMNW37lKDBDKJiLEvTmWncw7oumcXBykKarIK2aZcp3qWY2XXrt-MDXRWdImJZV_UsrtIrfzLVmfgo6ubv8-xjd_OANDCBkIRVyHuF-hvk97zXbK7I4jcoKUjgWWx0k2AyexyLHNDUWQQ3N3OR2czscvBEHoX-d8EAmRcnmoEDLKZSjBZTquJ8LHyUI9u58ymteXn6cx9H1Hzb2wM_dz4sdauuXnhMSzvDtkAL7kex6PrNosRHjZp4XPmpcXSxcp-r_k3B_B_3f7NCWOf1RuM_CDG76G0JW7uRS-OhuX4RnsW9XnGy4nZysKf6zz=w1261-h916?auditContext=forDisplay",
-    imageMatrix: [
-      [
-        "https://dummyimage.com/600x750/1a1a1a/ffffff",
-        "https://dummyimage.com/600x750/1a1a1a/ffffff",
-      ],
-      ["https://dummyimage.com/1200x700/1a1a1a/ffffff"],
-      [
-        "https://dummyimage.com/400x500/1a1a1a/ffffff",
-        "https://dummyimage.com/400x500/1a1a1a/ffffff",
-        "https://dummyimage.com/400x500/1a1a1a/ffffff",
-      ],
-    ],
-  },
-  {
-    id: 3,
-    title: "Claus Cabelereiro",
-    category: "Identidade Visual Cabelereiro",
-    image: "https://lh3.googleusercontent.com/rd-d/ALs6j_G7J5kIxjtvhVAN2yegnlRlCYgPgVYvD13XMU31fXOGhPLWX7n5M-0H6E7vVG4AJOYvgCwzHiqKOXlpn5fKeEWJyBppYNqsvnfHgLVawlP_naVMx-MoJGXTTXwJ9ybazPMSeQlxRtsCK-QijPYaPCheoyK3a-hR4Dp6rRHcfNTpCgs5e5PiZFG9LRtU-GjoXeyyUHY3s0S-DFzGkpn_EzkxgGW4nRGVlDsNM8pCFU_ZburD5GVsVE_MrPQz1Rfx4YqBDdb_-ICEXC7suafjmkd_4pHVX_qj0Jqb6fFt7NOoKO77SEZJqjPWrEu4c2NIM_J-LoiBjxw3TFdaB6ChkTgOdYs3wuNxyByo1DD0ywaDcGhb9tHFz_vzKSKmL-yDNwRdW5Wehnf7xcj6v3WTeNXSsfibxx5te7hdtIT1Rjs-LEF07JUbAMT52AGNkSFPtx4m8s1JWsghTlPRO5UPQQlUyRetE26H8rLzNMXrNYBxZU0Nt4ckUT_CSup1g6WwGxufTgYlPafProHyXifLzk4JGwSimACl0v79UUEXEhDZ_YHV97niLlfVn9GCBfSTVVbVbo61E4U1fiOiCA_eAfRBYG82Nlpzhe6LzVhoUq8My7TCa9sBjr3AITeNVk84uzgyUKmNDsesevsZePbNfX_qnL5UnWEdt4FhW9hFL1k3TJqLK4L7BYbfHxh3CcxPS1fhU2ZnFBeZSKu7ga5CGJevWpsD8yqw097MAziZXCS4y2P5r4z_qSpaROPFkMQhGo7Ckj0jH_WHpjNV4psf8uvPf6L6P7gq5S6BfSsqU8Mx9w7Uh4LODrUBskgEcQvA8-emAOTELtDR_mgGF3jnOZfsRyKIWHGeHlnonm6DDtxq8cbAduN7rPxmr2Kk9K1smMN6FcYMwJE-UP_mTmHOOSR663mX0HPhxmSiN5BGO0RluPUza7aWXuqUunRfcebozB9JfN18hMINeWsZ0xMni0ncqyBBnHGiIRaKpGeRC5DiFR-lgNI6g40t6rgNlBpL6a591gN3jJPGyhvHmdyhpZBQoGRN56YJjNUApexlsqBY=w1261-h916?auditContext=forDisplay",
-    imageMatrix: [
-      ["https://dummyimage.com/1200x750/1a1a1a/ffffff"],
-      [
-        "https://dummyimage.com/600x650/1a1a1a/ffffff",
-        "https://dummyimage.com/600x650/1a1a1a/ffffff",
-      ],
-      ["https://dummyimage.com/1200x680/1a1a1a/ffffff"],
-    ],
-  },
-  {
-    id: 4,
-    title: "Paulo Rodrigo | Visagista",
-    category: "Identidade Visual Visagismo",
-    image: "https://lh3.googleusercontent.com/rd-d/ALs6j_GO8GZwwyIdvIn7AW_ASv4olJCL-oimEiyjGkKAEP4rCF54AEiWuvG5b1pVySGHHUxbB5wONVT83MUbNaZBt9jG6z84iaz3-Gsze69e6BnO0JT4VkdzSm2wqKnDHhEXE0pzs0qRiTBfeUcQ-FiDHFLCCg76n_af1Q8x8M59OTlO7U8ZL_b3S_c1u1m1oNT4EXhyY3SwguxxzR14sFhxKZbHhzHYbPAxETzcheu_gBKdbagduAxH9RzK-7Y-QmF8IvR8RZwwW0KQwVDtPVxIZyqcqHU0fx-dqej6iPhS9g87v7vaLmfySdoYJf3eHk6rJWX5H9-G2ZYaMhbPX3agvSWKfod4VGn5kyBwjmiYhXokLa46mgwMkUl56gP0TXCff76VC7LHukNgGDeFz9FFIXDaYcwwhM_MQvOQSa9xLIKC5FNl64sQunrSSqkAnvWqgE3rEkLOQ0T81c5h_Ci0VGTtjFePBHh93LsV7jb4snm2WCMk_ZNSb7f9EqLhXeQZp_Ogw-T_2bwcwAVWa7AvQiR3TEVnn_Qp9u_WCBB2KorDzbWN9Qbg-_mOJWZjtojt87Ir7GEHl3a4UgVGMICGs_xI__1C1SuurJ_UlgdHHj7WawwQRGgrQrbvEqVYKa1MkkeBj5iz8JNodLyCqzHPC_0l-wMIJIcnq1q4Ycko4bB9cRCd9zTmLKxsqrhuDqCol6Jc2IQzMJJCUVMk3W60WJUbRYNf-InxKPs87TdM6cBPbKLd0lHyGItXk8_b1Il8-Bf7uAxMZHqhF8mGp3kYyS9TOufFbuFjWJ3OqGcOm92NcgnkVQyWD0_0CL5VOjnqlJxRcp7JvREp7WHEdu1EUMgZhSSQOlqAojo1Xdln_f_lJ_eusq54KHcs2PX53IE444Q4ncnuIywLJeYFOBNf58-aLgBJZ4ZR63-tkNq8WBIoG9gWYMwSn2nsrgv1FjUhcfWV1KTLNujD3TlzSOD_sQD-ZpPyrqt5KiurDcpMwXisU2kxrYf5s6-am6RjiIMBOj1k_5Xq8D-MvJBDBRnm3CutU-yMmykCTCsLVq7WSp53=w1261-h916?auditContext=forDisplay",
-    imageMatrix: [
-      [
-        "https://dummyimage.com/400x500/1a1a1a/ffffff",
-        "https://dummyimage.com/400x500/1a1a1a/ffffff",
-        "https://dummyimage.com/400x500/1a1a1a/ffffff",
-      ],
-      ["https://dummyimage.com/1200x700/1a1a1a/ffffff"],
-      [
-        "https://dummyimage.com/600x750/1a1a1a/ffffff",
-        "https://dummyimage.com/600x750/1a1a1a/ffffff",
-      ],
-    ],
-  },
-];
+const projects = projectsService.getAll();
 
 export default function PortfolioGallerySection() {
   const [selectedProject, setSelectedProject] = useState(null);
